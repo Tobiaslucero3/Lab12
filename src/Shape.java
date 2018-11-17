967b14d578827103f363626226aa4679d47c4170
Tobias Lucero <Tobias.N.Lucero-1@ou.edu>
2018-11-17 10:40:51 -0600

    Completed shape class

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,38 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
 
-public class Shape {
+public abstract class Shape implements Drawable
+{
+	protected Point[] location;
+	
+	private Color color;
+	
+	private boolean filled;
+	
+	public Shape(Color color, boolean filled)
+	{
+		this.color = color;
+		this.filled = filled;
+	}
+	
+	public Color getColor()
+	{
+		return this.color;
+	}
+	
+	public boolean isFilled()
+	{
+		return this.filled;
+	}
+	
+	public Point[] getLocation()
+	{
+		return this.location;
+	}
+
+	@Override
+	public abstract void draw(Graphics g);
 
 }
+

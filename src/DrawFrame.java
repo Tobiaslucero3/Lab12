f8a5c1ab8db1533a96a662d55baa005238d18689
Tobias Lucero <Tobias.N.Lucero-1@ou.edu>
2018-11-17 10:47:56 -0600

    Completed draw frame class

--- src/DrawFrame.java
+++ src/DrawFrame.java
@@ -36,9 +36,34 @@ public class DrawFrame extends JFrame
         // TODO: draw a dog:
         
         // Base head:
-        Circle base = new Circle(new Point(400, 300), 400, Color.ORANGE, true);
+        Circle base = new Circle(new Point(400, 300), 700, Color.BLACK, false);
+        Square otherBase = new Square(new Point(400,300),500, Color.BLUE,false);
         
+        Circle head = new Circle( new Point(400,300),450, Color.DARK_GRAY, true);
+        
+        RightTriangle rightEar = new RightTriangle(new Point(225,175),50,-150, Color.DARK_GRAY, true);
+        RightTriangle leftEar = new RightTriangle(new Point(575,175),-50,-150, Color.DARK_GRAY, true);
+        RightTriangle innerRightEar = new RightTriangle(new Point(230,175),30,-125, Color.PINK, true);
+        RightTriangle innerLeftEar = new RightTriangle(new Point(570,175),-30,-125, Color.PINK, true);
+        
+        Oval leftEyeBase = new Oval(new Point(300,225),60,95,Color.white,true);
+        Oval rightEyeBase = new Oval(new Point(500,225),60,95,Color.white,true);
+        Oval leftPupil = new Oval(new Point(300,225),45,70,Color.BLACK,true);
+        Oval rightPupil = new Oval(new Point(500,225),45,70,Color.BLACK,true);
+        
+        Circle nose = new Circle(new Point(400,300),50,Color.black,true);
+        
+        PolyLine leftMouth = new PolyLine(new Point(275,400), new Point(400,450), 67, Color.black, true);
+        PolyLine rightMouth = new PolyLine(new Point(525,400), new Point(400,450), 67, Color.black, true);
+        
+        Oval tongue = new Oval(new Point(445,450),60,90,Color.RED,true);
+        
+        Oval collar = new Oval(new Point(400,525),400,50,Color.BLUE,true);
+        
+        Circle buttonBase = new Circle(new Point(400,525),75,Color.red,true);
+        Circle buttonOutline = new Circle(new Point(400,525),52,Color.green,false);
         // Ears:
+        
         
         // Eyes:
         
@@ -52,10 +77,28 @@ public class DrawFrame extends JFrame
         drawPanel = new DrawPanel();
         
         // TODO: add shapes to the panel:
+        drawPanel.addShape(otherBase);
         drawPanel.addShape(base);
+        drawPanel.addShape(head);
+        drawPanel.addShape(rightEar);
+        drawPanel.addShape(leftEar);
+        drawPanel.addShape(innerRightEar);
+        drawPanel.addShape(innerLeftEar);
+        drawPanel.addShape(leftEyeBase);
+        drawPanel.addShape(rightEyeBase);
+        drawPanel.addShape(leftPupil);
+        drawPanel.addShape(rightPupil);
+        drawPanel.addShape(nose);
+        drawPanel.addShape(leftMouth);
+        drawPanel.addShape(rightMouth);
+        drawPanel.addShape(tongue);
+        drawPanel.addShape(collar);
+        drawPanel.addShape(buttonBase);
+        drawPanel.addShape(buttonOutline);
+        
         
         // set background color
-        drawPanel.setBackground(Color.YELLOW);
+        drawPanel.setBackground(Color.cyan);
         
         // add panel to frame
         this.add(drawPanel);
